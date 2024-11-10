364e3ad2f2e46e010e2d59aacf8ed68a056bac94
hieutt39 <mrhieutt39@gmail.com>
2024-11-10 15:10:48 +0700

    update video

--- src/modules/tools/urls.py
+++ src/modules/tools/urls.py
@@ -3,11 +3,12 @@ from django.urls import path, re_path
 from . import web
 
 urlpatterns = [
-    path('', web.index, name="web_index"),
+    path('', web.index, name="home"),
     path('quiz', web.quiz_index, name="quiz_index"),
     re_path(r'kptgmt/pdf/(?P<id>\w+)/$', web.kptgmt_pdf, name="web_kptgmt_pdf"),
     re_path(r'ndccmt/video/$', web.ndccmt_video, name="web_ndccmt_video"),
     re_path(r'thcmt/video/$', web.thcmt_video, name="web_thcmt_video"),
     re_path(r'thcmt/pdf/(?P<id>\w+)/$', web.thcmt_pdf, name="web_thcmt_pdf"),
     re_path(r'knpcmt/pdf/(?P<id>\w+)/$', web.knpcmt_pdf, name="web_knpcmt_pdf"),
+    re_path(r'library/digital/pdf/(?P<id>\w+)/$', web.library_library, name="library_library"),
 ]
